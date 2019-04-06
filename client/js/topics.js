557a75e564c1c6c19547fd459936a2c8f98227c5
asherLZR <asher.lzrz@gmail.com>
2019-04-07 00:02:11 +1100

    Merge server.js and add topics

---
+++ client/js/topics.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        hello: "hello",
+        topics: ["Units and Measurements", "The Scientific Process","The Big Bang, Elements and Radiation", 
+        "Discovery of the Galaxy and the Vastness of Space", "Age and Origin of the Solar System",
+        "Methods of Observational Astronomy", "The Life-Giving Sun -The Electromagnetic Spectrum",
+        "Planets of the Solar System", "The Earth in Space"]
+    }
+})
